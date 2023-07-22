de4a1d30223f7433df2ca79c78e185116b52d448
Aryan Singh <74867817+aryansingh920@users.noreply.github.com>
2023-07-22 19:27:49 +0530

    2023-07-22 19:27:49

---
+++ git.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+git add .
+current_date=$(date +%Y-%m-%d)
+current_time=$(date +%H:%M:%S)
+
+current_datetime="$current_date $current_time"
+git commit -m "$current_datetime"
+git push -u origin main 
