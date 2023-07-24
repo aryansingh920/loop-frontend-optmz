93ce4dc91926134555e1aa256dd5539222a8c57c
Aryan Singh <74867817+aryansingh920@users.noreply.github.com>
2023-07-24 14:41:51 +0530

    2023-07-24 14:41:51

--- git.sh
+++ git.sh
@@ -6,3 +6,6 @@ current_time=$(date +%H:%M:%S)
 current_datetime="$current_date $current_time"
 git commit -m "$current_datetime"
 git push -u origin main 
+
+echo "Deploying to vercel"
+vercel --prod
