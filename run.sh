a4faafc0a60554508c463a0d470f6c607c2eb3a6
Aryan Singh <74867817+aryansingh920@users.noreply.github.com>
2023-07-25 01:16:11 +0530

    2023-07-25 01:16:11

--- run.sh
+++ run.sh
@@ -1,3 +1,13 @@
-npm i
-npm start
-npm exec -- npm run storybook -- --initial-path=/onboarding --quiet
+#!/bin/bash
+
+# Install dependencies
+npm install
+
+# Start the React app in the background
+npm start &
+
+# Wait for a few seconds (adjust the time if needed)
+sleep 5
+
+# Run the Storybook in the background
+npm exec -- npm run storybook -- --initial-path=/onboarding --quiet &
